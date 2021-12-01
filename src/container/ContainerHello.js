1747550928c2d548194a8709fbfa055e2de8a94f
arnugroho <nugroho.akbar.r@gmail.com>
2021-12-01 15:59:51 +0700

    state example

--- src/container/ContainerHello.js
+++ src/container/ContainerHello.js
@@ -7,7 +7,13 @@ class ContainerHello extends React.Component {
     }
 
     changeName = () => {
+        console.log('changename')
         this.setState({name: "nugroho"});
+    }
+
+    changeName2 = () => {
+        console.log('changeName2')
+        this.setState({name: "syafiq"});
     }
 
     render() {
@@ -15,6 +21,7 @@ class ContainerHello extends React.Component {
             <div>
                 <h1>Hello {this.state.name}</h1>
                 <button onClick={this.changeName}>Change Name</button>
+                <button onClick={this.changeName2}>Change Name 2</button>
             </div>
         )
     }
