84ecf262187f66f171fd090c7c475bea028c18c5
arnugroho <nugroho.akbar.r@gmail.com>
2023-11-11 14:57:35 +0700

    update package json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,12 +8,11 @@ import ContainerHello from "./container/ContainerHello";
 import ComponentCount from "./component/ComponentCount";
 import ContainerCount from "./container/ContainerCount";
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/*<Greeting name = "akbar"/>*/}
-      <ContainerCount/>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
